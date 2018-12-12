d60e7688517ecd2f921383f5e712ca5e5261d729
Marc Chantreux <khatar@phear.org>
2018-12-12 16:50:56 +0100

    add doc and option a RELEASE_TESTING option
    * autoinstall dependencies
    * RELEASE_TESTING=${1:-} (with explainations in comments)

--- xt/bin/test_install_dist.sh
+++ xt/bin/test_install_dist.sh
@@ -1,2 +1,7 @@
+# with no parameter, the script start the regular test suite
+# with $1 not equal to '' or '0', start the whole test suite for CPAN releaes purpose
+
+
+dzil authordeps|cpanm
 rm LICENSE MANIFEST README Makefile.PL META.json META.yml 2>/dev/null
-dzil test
+RELEASE_TESTING=${1:-} dzil test
